9760d5aa45ec9f50bc0a20eb79660f1069c05d9f
luanvmendes <luanvmendes2@gmail.com>
2018-02-21 22:10:49 -0300

    FAQ

--- FAQ/index.php
+++ FAQ/index.php
@@ -20,25 +20,30 @@
 			HEADER
 			**************************************-->
 		<?php include('../includes/menu-principal.html');?>
+		<div class="container-fluid">
+	      	<img class="img-fluid mb-3 d-block mx-auto" src="../img/FAQ/duvidas.jpg" alt="FAQ">
+          </div>
 
 		<!--**************************************
 			FAQ
 			**************************************-->
-		<div class="container">
+		<div class="container my-4">
 			<h2>Dúvidas Frequentes</h2>
 			<div id="accordion">
 			  <div class="card">
 			    <div class="card-header" id="headingOne">
 			      <h5 class="mb-0">
 			        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-			          Collapsible Group Item #1
+			          Como faço para entrar em contato com o Lar São Vicente de Paulo(Sorocaba)?
 			        </button>
 			      </h5>
 			    </div>
 
 			    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
 			      <div class="card-body">
-			        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+			      	Você pode entrar em contato por meio dos telefones <b>(15) 3313-2025</b> ou <b>(15) 3313-9100</b>, 
+			      	pelo nosso email <b>sorocaba.larsaovicentedepaulo@gmail.com</b> ou vindo pessoalmente no endereço 
+			        <b>Avenida Betânia, 1255 - Jardim Betânia &emsp; Sorocaba - SP &emsp; CEP: 18071-590</b>
 			      </div>
 			    </div>
 			  </div>
@@ -46,13 +51,13 @@
 			    <div class="card-header" id="headingTwo">
 			      <h5 class="mb-0">
 			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-			          Collapsible Group Item #2
+			          Como faço para ser um voluntário?
 			        </button>
 			      </h5>
 			    </div>
 			    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
 			      <div class="card-body">
-			        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+			        Você pode conferir na página <a href="../voluntario">Voluntários</a>.
 			      </div>
 			    </div>
 			  </div>
@@ -60,13 +65,111 @@
 			    <div class="card-header" id="headingThree">
 			      <h5 class="mb-0">
 			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-			          Collapsible Group Item #3
+			          Qualquer um pode visitar o Lar?
 			        </button>
 			      </h5>
 			    </div>
 			    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
 			      <div class="card-body">
-			        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+			        Sim, desde que haja um agendamento antecipado via telefone - <b>(15) 3313-2025</b> ou <b>(15) 3313-9100</b>, ou pelo email <b>sorocaba.larsaovicentedepaulo@gmail.com</b> - lembrando que os horários de visitação são de <b>Segunda a Sábado das 09h as 11h e das 14h as 17h</b>.
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingFour">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+			          Tenho interesse em levar um idoso ao Lar, como proceder?
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+			      <div class="card-body">
+			        Primeiro, você deve verificar se atende aos critérios dispostos na página <a href="../acolhimento">Acolhimento</a>, depois poderá entrar em contato conosco.
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingFive">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+			          Como posso contribuir com o Lar?
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+			      <div class="card-body">
+			        Na página <a href="../doacoes">Doações</a> você encontrará detalhes de como contribuir com o Lar São Vicente de Paulo.
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingSix">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+			          Como nossos idosos são cuidados pelo Lar?
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseSix" class="collapse" aria-labelledby="headingSix" data-parent="#accordion">
+			      <div class="card-body">
+			        Na página <a href="../quem-somos">Quem Somos</a> você conhecerá um pouco dos nossos valores e a estrutura oferecida aos nossos queridos velhinhos.
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingSeven">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+			          Pergunta
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseSeven" class="collapse" aria-labelledby="headingSeven" data-parent="#accordion">
+			      <div class="card-body">
+			        Resposta
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingEight">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+			          Pergunta
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseEight" class="collapse" aria-labelledby="headingEight" data-parent="#accordion">
+			      <div class="card-body">
+			        Resposta
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingNine">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+			          Pergunta
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseNine" class="collapse" aria-labelledby="headingNine" data-parent="#accordion">
+			      <div class="card-body">
+			        Resposta
+			      </div>
+			    </div>
+			  </div>
+			  <div class="card">
+			    <div class="card-header" id="headingTen">
+			      <h5 class="mb-0">
+			        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+			          Pergunta
+			        </button>
+			      </h5>
+			    </div>
+			    <div id="collapseTen" class="collapse" aria-labelledby="headingTen" data-parent="#accordion">
+			      <div class="card-body">
+			        Resposta
 			      </div>
 			    </div>
 			  </div>
