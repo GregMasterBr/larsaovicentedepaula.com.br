4f69772c862bccd960d579ecaf98e238a9c4769b
AnneGabriele <gabrieleagt@gmail.com>
2018-03-02 22:03:27 -0300

    alteracao da pagina eventos, ajuste do calendario para dispositivos moveis

--- eventos/index.php
+++ eventos/index.php
@@ -29,7 +29,12 @@
         </div>
       </section>
 
- 		<iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=sorocaba.larsaovicentedepaulo%40gmail.com&amp;color=%231B887A&amp;src=pt.brazilian%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FSao_Paulo" style="border-width:0 ; margin: 0 2% 50px 2%" width="96%" height="600" frameborder="0" scrolling="no"></iframe>
+      <div class="agenda-desk">
+ 		     <iframe src="https://calendar.google.com/calendar/b/1/embed?showTitle=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=sorocaba.larsaovicentedepaulo%40gmail.com&amp;color=%231B887A&amp;src=pt.brazilian%23holiday%40group.v.calendar.google.com&amp;color=%23125A12&amp;ctz=America%2FSao_Paulo" style="border-width:0 ; margin: 0 2% 50px 2%" width="96%" height="600" frameborder="0" scrolling="no"></iframe>
+      </div>
+      <div class="agenda-cel">
+        <iframe src="https://calendar.google.com/calendar/embed?showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=sorocaba.larsaovicentedepaulo%40gmail.com&amp;color=%231B887A&amp;ctz=America%2FSao_Paulo" style="border-width:0" frameborder="0" scrolling="no"></iframe>
+      </div>
 
  	</main>
 
