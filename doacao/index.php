a2eaa36d146eb8821b87ed7c4241fc464c609622
luanvmendes <luanvmendes2@gmail.com>
2018-03-11 00:45:16 -0300

    Padronizando fontes/header

--- doacao/index.php
+++ doacao/index.php
@@ -21,51 +21,53 @@
  
  	<!--Album-->
  	
- 	<main role="main">
+ 	  <main role="main">
 
-      <section class="jumbotron text-center">
+      <section class="jumbotron mb-0 pb-0 text-center text-white bg-img">
         <div class="container">
-          <h1 class="jumbotron-heading">Doação</h1>
-          <p class="lead text-muted">O que fazemos por nós mesmos morre conosco. O que fazemos pelos outros permanece e é imortal. (Albert Pine)</p>
+          <h1 class="jumbotron-heading text-uppercase mb-0 shadow">Doação</h1>
+          <p class="p-header mb-3 shadow">O que fazemos por nós mesmos morre conosco. O que fazemos pelos outros permanece e é imortal. (Albert Pine)</p>
         </div>
       </section>
 
-    <div class="box">
-      <h2>DOAÇÕES PELO IMPOSTO DE RENDA</h2>
-      <p>Você sabia que poderá destinar uma parte do dinheiro que deve ao imposto de renda para o Lar São Vicente de Paulo?
-Ao invés de dar dinheiro para o imposto de renda, doe para a caridade!<br>
-Mas, atenção!<br>
-Para deduzir da declaração de abril de 2017, as doações devem ser efetuadas até o dia 30/12/2016.<br>
-Não se trata de deixar de pagar imposto, mas sim de escolher a destinação do Imposto. E essa escolha é sua!!! <br>
-Pela lei, você poderá deduzir até 6% do imposto que deve pagar ao governo e depositar o valor ao Fundo Municipal do Idoso, que o repassará ao Lar São Vicente. A dedução poderá ser efetuada na declaração de abril de 2017, desde que a faça pelo modelo completo. Isso resulta num menor imposto a pagar ou num maior valor a restituir, porque o cálculo é sobre o imposto devido, não sobre o imposto a recolher.<br>
-<br>
-Como funciona <br>
-Por exemplo: Se você tem um imposto devido de R$ 5.000,00, poderá direcionar R$ 300,00 para o Lar São Vicente. Quando fizer a declaração, esse valor será abatido do imposto devido, o que acarretará um IR menor a recolher ou uma restituição maior. <br>
-<br>
-Para estimar o valor do imposto devido, busque a sua declaração do ano passado. Ela lhe dará uma idéia de quanto poderá depositar ao Fundo Municipal do Idoso.<br>
-A doação deverá se feita até o dia 30 de dezembro de 2016 na conta do Fundo Municipal do Idoso nº 83000-3, agência 0032-9 do Banco do Brasil.<br>
-Feito o depósito, ligue para o nº 3222-5130 para que peguem o comprovante de depósito bancário com você ou entregue-o no Lar São Vicente, que providenciará o recibo do Fundo. Ele será o documento hábil para o abatimento na sua declaração do IR e deverá ser guardado por 5 anos<br>
-<br>
-Exercitar a cidadania<br>
-Se você não faz isso, deixará para o governo decidir o que fazer com o seu dinheiro. É uma questão de exercitar a cidadania.<br>
-<br>
-Pessoas Jurídicas<br>
-As PJ também poderão ajudar. O limite da dedução é 1% do IR devido em cada período de apuração, conforme estabelecido no Decreto nº 794/93. A única exigência é que sejam tributadas pelo lucro real.<br>
-<br>
-A empresa doadora receberá um certificado de responsabilidade social como “Empresa Amiga do Lar São Vicente de Paulo”, que poderá utilizar em sua rede de comunicação.
-Como lançar os valores na declaração.<br>
-Ao preencher a declaração, as doações deverão ser informadas na ficha “Doações efetuadas” com valores e o CNPJ nº 21.566.761/0001-79, do Fundo Municipal do Idoso. O programa calcula o imposto devido e faz a dedução dos valores doados.<br>
-<br>
-Você não terá prejuízo algum, apenas fará um adiantamento. Mas terá muitas bênçãos, tenha certeza!!!</p>
-    </div>
- 		
-
- 	</main>
+      <div class="container my-4">
+        <h2>DOAÇÕES PELO IMPOSTO DE RENDA</h2>
+        <p>
+          Você sabia que poderá destinar uma parte do dinheiro que deve ao imposto de renda para o Lar São Vicente de Paulo?
+          Ao invés de dar dinheiro para o imposto de renda, doe para a caridade!<br>
+          Mas, atenção!<br>
+          Para deduzir da declaração de abril de 2017, as doações devem ser efetuadas até o dia 30/12/2016.<br>
+          Não se trata de deixar de pagar imposto, mas sim de escolher a destinação do Imposto. E essa escolha é sua!!! <br>
+          Pela lei, você poderá deduzir até 6% do imposto que deve pagar ao governo e depositar o valor ao Fundo Municipal do Idoso, que o repassará ao Lar São Vicente. A dedução poderá ser efetuada na declaração de abril de 2017, desde que a faça pelo modelo completo. Isso resulta num menor imposto a pagar ou num maior valor a restituir, porque o cálculo é sobre o imposto devido, não sobre o imposto a recolher.<br>
+          <br>
+          Como funciona <br>
+          Por exemplo: Se você tem um imposto devido de R$ 5.000,00, poderá direcionar R$ 300,00 para o Lar São Vicente. Quando fizer a declaração, esse valor será abatido do imposto devido, o que acarretará um IR menor a recolher ou uma restituição maior. <br>
+          <br>
+          Para estimar o valor do imposto devido, busque a sua declaração do ano passado. Ela lhe dará uma idéia de quanto poderá depositar ao Fundo Municipal do Idoso.<br>
+          A doação deverá se feita até o dia 30 de dezembro de 2016 na conta do Fundo Municipal do Idoso nº 83000-3, agência 0032-9 do Banco do Brasil.<br>
+          Feito o depósito, ligue para o nº 3222-5130 para que peguem o comprovante de depósito bancário com você ou entregue-o no Lar São Vicente, que providenciará o recibo do Fundo. Ele será o documento hábil para o abatimento na sua declaração do IR e deverá ser guardado por 5 anos<br>
+          <br>
+          Exercitar a cidadania<br>
+          Se você não faz isso, deixará para o governo decidir o que fazer com o seu dinheiro. É uma questão de exercitar a cidadania.<br>
+          <br>
+          Pessoas Jurídicas<br>
+          As PJ também poderão ajudar. O limite da dedução é 1% do IR devido em cada período de apuração, conforme estabelecido no Decreto nº 794/93. A única exigência é que sejam tributadas pelo lucro real.<br>
+          <br>
+          A empresa doadora receberá um certificado de responsabilidade social como “Empresa Amiga do Lar São Vicente de Paulo”, que poderá utilizar em sua rede de comunicação.
+          Como lançar os valores na declaração.<br>
+          Ao preencher a declaração, as doações deverão ser informadas na ficha “Doações efetuadas” com valores e o CNPJ nº 21.566.761/0001-79, do Fundo Municipal do Idoso. O programa calcula o imposto devido e faz a dedução dos valores doados.<br>
+          <br>
+          Você não terá prejuízo algum, apenas fará um adiantamento. Mas terá muitas bênçãos, tenha certeza!!!
+        </p>
+      </div>
+    </main>
 
     <!-- FOOTER -->
     <?php 
        include('../includes/footer.html');
     ?>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
   </body>
 
 
