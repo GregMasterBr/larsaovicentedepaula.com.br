0723431f91d67de868a95788160e302af4cd427f
Tamires <ft.tamires@gmail.com>
2018-03-16 16:11:37 -0300

    Alterei o texto de doação e inseri a foto como background principal

--- doacao/index.php
+++ doacao/index.php
@@ -11,18 +11,18 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="../css/estilo.css" />
 		<link rel="stylesheet" type="text/css" href="../css/doacao.css" />
-    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">    
+    <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
 	</head>
+  <body>
 
-	<body>
+    <!--**************************************
+      HEADER
+      **************************************-->
    	<?php
    	include ('../includes/menu-principal.php');
     ?>
  
- 	<!--Album-->
- 	
  	  <main role="main">
-
       <section class="jumbotron mb-0 pb-0 text-center text-white bg-img">
         <div class="container">
           <h1 class="jumbotron-heading text-uppercase mb-0 shadow">Doação</h1>
@@ -30,22 +30,25 @@
         </div>
       </section>
 
+<!--**************************************
+      TIMELINE
+      **************************************-->
       <div class="container my-4">
         <h2>DOAÇÕES PELO IMPOSTO DE RENDA</h2>
         <p>
           Você sabia que poderá destinar uma parte do dinheiro que deve ao imposto de renda para o Lar São Vicente de Paulo?
           Ao invés de dar dinheiro para o imposto de renda, doe para a caridade!<br>
           Mas, atenção!<br>
-          Para deduzir da declaração de abril de 2017, as doações devem ser efetuadas até o dia 30/12/2016.<br>
+          Para deduzir da declaração de abril de 2019, as doações devem ser efetuadas até o dia 30/12/2018.<br>
           Não se trata de deixar de pagar imposto, mas sim de escolher a destinação do Imposto. E essa escolha é sua!!! <br>
-          Pela lei, você poderá deduzir até 6% do imposto que deve pagar ao governo e depositar o valor ao Fundo Municipal do Idoso, que o repassará ao Lar São Vicente. A dedução poderá ser efetuada na declaração de abril de 2017, desde que a faça pelo modelo completo. Isso resulta num menor imposto a pagar ou num maior valor a restituir, porque o cálculo é sobre o imposto devido, não sobre o imposto a recolher.<br>
+          Pela lei, você poderá deduzir até 6% do imposto que deve pagar ao governo e depositar o valor ao Fundo Municipal do Idoso, que o repassará ao Lar São Vicente. A dedução poderá ser efetuada na declaração de abril de 2019, desde que a faça pelo modelo completo. Isso resulta num menor imposto a pagar ou num maior valor a restituir, porque o cálculo é sobre o imposto devido, não sobre o imposto a recolher.<br>
           <br>
           Como funciona <br>
           Por exemplo: Se você tem um imposto devido de R$ 5.000,00, poderá direcionar R$ 300,00 para o Lar São Vicente. Quando fizer a declaração, esse valor será abatido do imposto devido, o que acarretará um IR menor a recolher ou uma restituição maior. <br>
           <br>
           Para estimar o valor do imposto devido, busque a sua declaração do ano passado. Ela lhe dará uma idéia de quanto poderá depositar ao Fundo Municipal do Idoso.<br>
-          A doação deverá se feita até o dia 30 de dezembro de 2016 na conta do Fundo Municipal do Idoso nº 83000-3, agência 0032-9 do Banco do Brasil.<br>
-          Feito o depósito, ligue para o nº 3222-5130 para que peguem o comprovante de depósito bancário com você ou entregue-o no Lar São Vicente, que providenciará o recibo do Fundo. Ele será o documento hábil para o abatimento na sua declaração do IR e deverá ser guardado por 5 anos<br>
+          A doação deverá se feita até o dia 30 de dezembro de 2018 na conta do Fundo Municipal do Idoso nº xxxx-xx, agência xxxx-xx do Banco XXX.<br>
+          Feito o depósito, ligue para o nº xxxx-xxxx para que peguem o comprovante de depósito bancário com você ou entregue-o no Lar São Vicente, que providenciará o recibo do Fundo. Ele será o documento hábil para o abatimento na sua declaração do IR e deverá ser guardado por 5 anos.<br>
           <br>
           Exercitar a cidadania<br>
           Se você não faz isso, deixará para o governo decidir o que fazer com o seu dinheiro. É uma questão de exercitar a cidadania.<br>
@@ -55,7 +58,7 @@
           <br>
           A empresa doadora receberá um certificado de responsabilidade social como “Empresa Amiga do Lar São Vicente de Paulo”, que poderá utilizar em sua rede de comunicação.
           Como lançar os valores na declaração.<br>
-          Ao preencher a declaração, as doações deverão ser informadas na ficha “Doações efetuadas” com valores e o CNPJ nº 21.566.761/0001-79, do Fundo Municipal do Idoso. O programa calcula o imposto devido e faz a dedução dos valores doados.<br>
+          Ao preencher a declaração, as doações deverão ser informadas na ficha “Doações efetuadas” com valores e o CNPJ nº xxxxxxxxxx/0001-xx, do Fundo Municipal do Idoso. O programa calcula o imposto devido e faz a dedução dos valores doados.<br>
           <br>
           Você não terá prejuízo algum, apenas fará um adiantamento. Mas terá muitas bênçãos, tenha certeza!!!
         </p>
